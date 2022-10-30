b321f1b44403fbcb9500a6a23111f3cb9fa2bb54
Andrew Burkett <burkett.andrew@gmail.com>
2022-10-30 15:42:11 -0700

    Alternate implementation for optimizer
    This is an alternative implementation for the optimizer. I still don't
    love that it requires each optimizer implementation to update the input
    and push to the output correctly.

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,9 +1,9 @@
 use bpf_ins::{Instruction, Opcode};
 
-struct Optimizer {
-    pub num_instructions: usize,
-    pub function: fn(&[Instruction]) -> Option<Vec<Instruction>>,
-}
+/// An [`Optimizer`] takes a set of input instructions and pushes optimized
+/// instructions to the output (the second argument) if it succeeeds. On success
+/// it should return `true`.
+type Optimizer = fn(&mut &[Instruction], &mut Vec<Instruction>) -> bool;
 
 /// Makes the following optimization:
 ///
@@ -11,27 +11,39 @@ struct Optimizer {
 ///   r2 += N   |
 ///   r2 = *r2  |
 ///
-fn optimize_mov_add_load(ins: &[Instruction]) -> Option<Vec<Instruction>> {
+fn optimize_mov_add_load(inp: &mut &[Instruction], out: &mut Vec<Instruction>) -> bool {
+    const NEEDED: usize = 3;
+    if inp.len() < NEEDED {
+        return false;
+    }
+    let (ins, rem) = inp.split_at(NEEDED);
     let load_size = if let Opcode::Memory(memory) = ins[2].get_opcode() {
         *memory.get_size()
     } else {
-        return None;
+        return false;
+    };
+
+    let offset1: i16 = match ins[1].get_imm().try_into() {
+        Ok(offset) => offset,
+        Err(_) => return false,
     };
 
     let check0 = Instruction::movx64(ins[0].get_dst_reg(), ins[0].get_src_reg());
-    let check1 = Instruction::add64(ins[1].get_dst_reg(), ins[1].get_imm().try_into().ok()?);
+    let check1 = Instruction::add64(ins[1].get_dst_reg(), offset1.into());
     let check2 = Instruction::loadx(ins[2].get_dst_reg(), ins[2].get_src_reg(), 0, load_size);
 
     if check0 != ins[0] || check1 != ins[1] || check2 != ins[2] {
-        return None;
+        return false;
     }
 
-    Some(vec![Instruction::loadx(
+    *inp = rem;
+    out.push(Instruction::loadx(
         ins[0].get_dst_reg(),
         ins[0].get_src_reg(),
-        ins[1].get_imm().try_into().ok()?,
+        offset1,
         load_size,
-    )])
+    ));
+    true
 }
 
 ///
@@ -40,67 +52,66 @@ fn optimize_mov_add_load(ins: &[Instruction]) -> Option<Vec<Instruction>> {
 ///   r2 += N   | r2 = *(r2 + N)
 ///   r2 = *r2  |
 ///
-fn optimize_add_load(ins: &[Instruction]) -> Option<Vec<Instruction>> {
+// I would think these could return Option<&'static [Instruction]>
+fn optimize_add_load(inp: &mut &[Instruction], out: &mut Vec<Instruction>) -> bool {
+    const NEEDED: usize = 2;
+    if inp.len() < NEEDED {
+        return false;
+    }
+    let (ins, rem) = inp.split_at(NEEDED);
     let load_size = if let Opcode::Memory(memory) = ins[1].get_opcode() {
         *memory.get_size()
     } else {
-        return None;
+        return false;
     };
 
-    let check0 = Instruction::add64(ins[0].get_dst_reg(), ins[0].get_imm().try_into().ok()?);
+    let offset0: i16 = match ins[0].get_imm().try_into() {
+        Ok(offset) => offset,
+        Err(_) => return false,
+    };
+
+    let check0 = Instruction::add64(ins[0].get_dst_reg(), offset0.into());
     let check1 = Instruction::loadx(ins[1].get_dst_reg(), ins[1].get_src_reg(), 0, load_size);
 
     if check0 != ins[0] || check1 != ins[1] {
-        return None;
+        return false;
     }
 
-    Some(vec![Instruction::loadx(
+    *inp = rem;
+    out.push(Instruction::loadx(
         ins[0].get_dst_reg(),
         ins[0].get_dst_reg(),
-        ins[0].get_imm().try_into().ok()?,
+        offset0,
         load_size,
-    )])
+    ));
+    true
+}
+
+fn no_optimization(inp: &mut &[Instruction], out: &mut Vec<Instruction>) -> bool {
+    let (ins, rem) = match inp.split_first() {
+        Some((ins, rem)) => (ins, rem),
+        None => return false,
+    };
+    out.push(*ins);
+    *inp = rem;
+    true
 }
 
 /// List of optimizers used by the `optimize` function.
-const OPTIMIZERS: [Optimizer; 2] = [
-    Optimizer {
-        num_instructions: 3,
-        function: optimize_mov_add_load,
-    },
-    Optimizer {
-        num_instructions: 2,
-        function: optimize_add_load,
-    },
-];
+static OPTIMIZERS: [Optimizer; 3] = [optimize_mov_add_load, optimize_add_load, no_optimization];
 
 /// Applies various optimizations to the given list of instructions.
 ///
 /// # Arguments
 ///
 /// * `instructions` - The program, as a list of instructions, to optimize.
-pub fn optimize(instructions: &[Instruction]) -> Vec<Instruction> {
-    let mut num_eliminated = 0;
+pub fn optimize(mut instructions: &[Instruction]) -> Vec<Instruction> {
     let mut optimized = vec![];
-    'outer: for i in 0..instructions.len() {
-        let start = usize::min(instructions.len(), i + num_eliminated);
-        let remaining = &instructions[start..];
-        if remaining.is_empty() {
-            break;
+    let instructions = &mut instructions;
+    while !instructions.is_empty() {
+        for optimizer in OPTIMIZERS {
+            optimizer(instructions, &mut optimized);
         }
-
-        for optimizer in OPTIMIZERS
-            .iter()
-            .filter(|o| o.num_instructions <= remaining.len())
-        {
-            if let Some(mut instructions) = (optimizer.function)(remaining) {
-                optimized.append(&mut instructions);
-                num_eliminated += optimizer.num_instructions - 1;
-                continue 'outer;
-            }
-        }
-
-        optimized.push(remaining[0]);
     }
 
     optimized
